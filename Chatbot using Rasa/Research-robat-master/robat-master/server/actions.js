31bba85fc735f241f80a422fdb633770c3091dc3
3m6d <dipawolimalla333@gmail.com>
2024-10-23 14:26:49 +0545

    Remove virtual environment and other unnecessary files

--- Chatbot using Rasa/Research-robat-master/robat-master/server/actions.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  actions: {},
-  register(name, action) {
-    this.actions[name] = action;
-  },
-};
--- Chatbot using Rasa/Research-robat-master/robat-master/server/oba.js
+++
@@ -1,8 +0,0 @@
-const OBA = require('oba-api');
-
-const client = new OBA({
-  public: process.env.OBA_PUBLIC,
-  secret: process.env.OBA_SECRET,
-});
-
-module.exports = client;
